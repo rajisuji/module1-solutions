7f286acb61ff886f30994cd6cddcc89c639fda15
rajeswari <rajeswari.elogic@gmail.com>
2020-09-03 12:23:34 +0530

    assignment

--- app.js
+++ app.js
@@ -3,7 +3,16 @@
 angular.module('LunchCheck', [])
 .controller('LunchCheckController', LunchCheckController);
     function LunchCheckController($scope,$filter,$injector){
-$scope.name = ""; 
+$scope.name = "";
+$scope.state = "sunrise";
+$scope.upper = function (){
+    var upCase = $filter('uppercase');
+    $scope.name = upCase($scope.name);
+}
+$scope.imagechange = function(){
+$scope.state = "sunset";
+}
+//console.log($injector.annotate(LunchCheckController));
 
 
 }
